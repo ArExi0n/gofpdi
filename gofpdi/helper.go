4595f0b6c49c094e2cf5a4f353093a35bd15c1a8
ArExi0n <notbollt@gmail.com>
2026-08-18 20:33:31 +0530

    feat: helper components

--- gofpdi/helper.go
+++ gofpdi/helper.go
@@ -1,6 +1,8 @@
 package gofpdi
 
-import "strings"
+import (
+	"strings"
+)
 
 // Determine if a value is numeric
 func is_numeric(val interface{}) bool {
@@ -90,7 +92,37 @@ func abs(x int) int {
 	m := x >> (intSize - 1)
 
 	// In twos complment representation, the negative number
-	// if condition {
-
+	// of any number (except the smallest one) can be computed
+	// by fliping all the bits and add 1.This is faster than
+	// code with a branch
+	// See hacker's delight, section 2-4.
 	return (x ^ m) - m
+}
+
+// filterPaeth applies the Paeth filter to the cdat slice.
+// cdat is the current row's data, pdat is the previous row's data.
+func filterPaeth(cdat, pdat []byte, bytesPerPixel int) {
+	var a, b, c, pa, pb, pc int
+	for i := 0; i < bytesPerPixel; i++ {
+		a, c = 0, 0
+		for j := i; j < len(cdat); j += bytesPerPixel {
+			b = int(pdat[j])
+			pa = b - c
+			pb = a - c
+			pc = abs(pa + pb)
+			pa = abs(pa)
+			pb = abs(pb)
+			if pa <= pb && pa <= pc {
+				// No-op
+			} else if pb <= pc {
+				a = b
+			} else {
+				a = c
+			}
+			a += int(cdat[j])
+			a &= 0xff
+			cdat[j] = uint8(a)
+			c = b
+		}
+	}
 }
